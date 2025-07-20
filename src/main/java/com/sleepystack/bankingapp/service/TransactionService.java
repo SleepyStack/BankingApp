e47bff7406c7db12be87632648d0bb1ddf5662ff
Chirag <chirag2006chopra@gmail.com>
2025-07-21 01:16:05 +0530

    Update TransactionService.java

--- src/main/java/com/sleepystack/bankingapp/service/TransactionService.java
+++ src/main/java/com/sleepystack/bankingapp/service/TransactionService.java
@@ -1,4 +1,33 @@
 package com.sleepystack.bankingapp.service;
 
+import com.sleepystack.bankingapp.model.Account;
+import com.sleepystack.bankingapp.model.Transaction;
+import com.sleepystack.bankingapp.repository.AccountRepository;
+import com.sleepystack.bankingapp.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
 public class TransactionService {
+    private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository, AccountRepository accountRepository) {
+        this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
+    }
+    public Transaction deposit(String accountId, double amount, String userId, String descrition){
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(() -> new IllegalArgumentException("Account not found"));
+        if(amount <= 0){
+            throw new IllegalArgumentException("Deposit amount must be greater than zero");
+        }
+        account.setBalance(account.getBalance() + amount);
+        accountRepository.save(account);
+        Transaction transaction = new Transaction(null, accountId, "deposit", amount, Instant.now(), null, "completed", userId, "Money Deposited via Cash Deposit Machine");
+        return transactionRepository.save(transaction);
+    }
 }
