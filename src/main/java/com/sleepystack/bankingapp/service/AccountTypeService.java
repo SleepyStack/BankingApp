15b7fe2d7122d5e65c55757c6db0c1b5bd92d5c4
Chirag <chirag2006chopra@gmail.com>
2025-07-20 19:30:07 +0530

    added UserService

--- src/main/java/com/sleepystack/bankingapp/service/AccountTypeService.java
+++ src/main/java/com/sleepystack/bankingapp/service/AccountTypeService.java
@@ -1,4 +1,8 @@
 package com.sleepystack.bankingapp.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountTypeService {
+
 }
--- src/main/java/com/sleepystack/bankingapp/service/UserService.java
+++ src/main/java/com/sleepystack/bankingapp/service/UserService.java
@@ -1,4 +1,33 @@
 package com.sleepystack.bankingapp.service;
 
+import com.sleepystack.bankingapp.repository.UserRepository;
+import com.sleepystack.bankingapp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+    public User getUser(String id){
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+    public User updateUser(String id, User updatedUser){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        updatedUser.setId(id);
+        return userRepository.save(updatedUser);
+    }
+    public void deleteUser(String id){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        userRepository.deleteById(id);
+    }
 }
