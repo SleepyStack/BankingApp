91fe3b6b3260ab4dc5fe05fc43f964dbeafcfa7f
Chirag <chirag2006chopra@gmail.com>
2025-07-21 01:00:27 +0530

    Create TransactionRepository.java

---
+++ src/main/java/com/sleepystack/bankingapp/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.sleepystack.bankingapp.repository;
+
+import com.sleepystack.bankingapp.model.Transaction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends MongoRepository<Transaction, String> {
+}
