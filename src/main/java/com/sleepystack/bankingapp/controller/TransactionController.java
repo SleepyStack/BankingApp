1c0536fde62eb53574cfc7485f1917ada3fe5e7c
Chirag <chirag2006chopra@gmail.com>
2025-07-22 03:23:55 +0530

    Update TransactionController.java

--- src/main/java/com/sleepystack/bankingapp/controller/TransactionController.java
+++ src/main/java/com/sleepystack/bankingapp/controller/TransactionController.java
@@ -1,7 +1,18 @@
 package com.sleepystack.bankingapp.controller;
 
+import com.sleepystack.bankingapp.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/transactions")
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    
 }
