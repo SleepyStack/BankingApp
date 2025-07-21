293d671ebae9661dcaa89a237a9bb9ada7f802e3
Chirag <chirag2006chopra@gmail.com>
2025-07-22 03:42:57 +0530

    Update TransactionController.java

--- src/main/java/com/sleepystack/bankingapp/controller/TransactionController.java
+++ src/main/java/com/sleepystack/bankingapp/controller/TransactionController.java
@@ -1,8 +1,11 @@
 package com.sleepystack.bankingapp.controller;
 
+import com.sleepystack.bankingapp.model.Transaction;
 import com.sleepystack.bankingapp.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,26 @@ public class TransactionController {
     public TransactionController(TransactionService transactionService) {
         this.transactionService = transactionService;
     }
-    
+    @PostMapping
+    public Transaction deposit(@RequestParam String accountId,
+                               @RequestParam double amount,
+                               @RequestParam String userId,
+                               @RequestParam(required = false) String description) {
+        return transactionService.deposit(accountId,amount,userId,description);
+    }
+    @PostMapping
+    public Transaction withdraw(@RequestParam String accountId,
+                                @RequestParam double amount,
+                                @RequestParam String userId,
+                                @RequestParam(required = false) String description) {
+        return transactionService.withdraw(accountId, amount, userId, description);
+    }
+    @PostMapping
+    public Transaction transfer(@RequestParam String fromAccountId,
+                                @RequestParam String toAccountId,
+                                @RequestParam double amount,
+                                @RequestParam String userId,
+                                @RequestParam(required = false) String description) {
+        return transactionService.transfer(fromAccountId, toAccountId, amount, userId, description);
+    }
 }
