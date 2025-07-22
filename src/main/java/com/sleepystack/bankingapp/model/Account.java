7c3340c8f17e81e84ea6eeeb2eea873a3a76ae01
Chirag <chirag2006chopra@gmail.com>
2025-07-23 04:02:39 +0530

    Update Account.java

--- src/main/java/com/sleepystack/bankingapp/model/Account.java
+++ src/main/java/com/sleepystack/bankingapp/model/Account.java
@@ -5,21 +5,23 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "accounts")
 public class Account {
     @Id
-    private String userId;
-
     private String id;
+
+    private String userId;
     private String accountNumber;
     private String accountTypeId;
     private double balance;
+    private String publicIdentifier;
 
-    public Account() {} // Default constructor required by Spring Data
+    public Account() {}
 
-    public Account(String userId, String id, String accountNumber, String accountTypeId, double balance) {
-        this.userId = userId;
+    public Account(String id, String userId, String accountNumber, double balance, String publicIdentifier, String accountTypeId) {
         this.id = id;
+        this.userId = userId;
         this.accountNumber = accountNumber;
-        this.accountTypeId = accountTypeId;
         this.balance = balance;
+        this.publicIdentifier = publicIdentifier;
+        this.accountTypeId = accountTypeId;
     }
 
     public String getId() {
@@ -28,6 +30,14 @@ public class Account {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getAccountNumber() {
@@ -54,11 +64,14 @@ public class Account {
         this.balance = balance;
     }
 
-    public String getUserId() {
-        return userId;
+    public String getPublicIdentifier() {
+        return publicIdentifier;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public void setPublicIdentifier(String publicIdentifier) {
+        this.publicIdentifier = publicIdentifier;
     }
 }
+
+
+
