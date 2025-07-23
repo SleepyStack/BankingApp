c69e21e18fce3f117a29b0450f93a2ad71b8bb5e
Chirag <chirag2006chopra@gmail.com>
2025-07-24 04:55:56 +0530

    Update TransactionRepository.java
    added two new querying methods

--- src/main/java/com/sleepystack/bankingapp/repository/TransactionRepository.java
+++ src/main/java/com/sleepystack/bankingapp/repository/TransactionRepository.java
@@ -4,6 +4,10 @@ import com.sleepystack.bankingapp.model.Transaction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
+    List<Transaction> findByAccountNumberOrderByTimestampDesc(String accountNumber);
+    List<Transaction> findByInitiatedByUserIdOrderByTimestampDesc(String userId);
 }
