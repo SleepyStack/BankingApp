17835223e6714c06d0f9428aadf57c9bcbf969fe
Chirag <chirag2006chopra@gmail.com>
2025-07-30 11:01:19 +0530

    Update JsonWebTokenSigningKey.java

--- src/main/java/com/sleepystack/bankingapp/util/JsonWebTokenSigningKey.java
+++ src/main/java/com/sleepystack/bankingapp/util/JsonWebTokenSigningKey.java
@@ -1,8 +1,17 @@
 package com.sleepystack.bankingapp.util;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
 
 @Component
 public class JsonWebTokenSigningKey {
+    private final SecretKey secretKey;
+    private final long expirationTime = 3600000; // 1 hour in milliseconds
 
+    public JsonWebTokenSigningKey(@Value("${jwt.secret}") String jwtSecret) {
+        this.secretKey = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+    }
 }
