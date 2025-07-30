5582b334de7a34a1a8c780bc7eae1c864cd53f92
Chirag <chirag2006chopra@gmail.com>
2025-07-30 07:18:16 +0530

    Update AuthController.java

--- src/main/java/com/sleepystack/bankingapp/controller/AuthController.java
+++ src/main/java/com/sleepystack/bankingapp/controller/AuthController.java
@@ -12,20 +12,17 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthController(UserService userService, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+    public AuthController(UserService userService, AuthenticationManager authenticationManager) {
         this.userService = userService;
-        this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
     }
     @PostMapping("/register")
