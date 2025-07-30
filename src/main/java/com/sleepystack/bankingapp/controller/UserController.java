eba457fdf9698fac76491765888b032552deea21
Chirag <chirag2006chopra@gmail.com>
2025-07-31 01:22:00 +0530

    Update UserController.java

--- src/main/java/com/sleepystack/bankingapp/controller/UserController.java
+++ src/main/java/com/sleepystack/bankingapp/controller/UserController.java
@@ -5,6 +5,7 @@ import com.sleepystack.bankingapp.dto.UpdateUserRequest;
 import com.sleepystack.bankingapp.model.User;
 import com.sleepystack.bankingapp.service.UserService;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @RestController
 
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private final UserService userService;
 
@@ -48,6 +50,7 @@ public class UserController {
     }
     @DeleteMapping("/{publicId}")
     public void deleteUser(@PathVariable String publicId){
+        log.info("Deleting user with public ID: {}", publicId);
         userService.deleteUserByPublicId(publicId);
     }
 }
