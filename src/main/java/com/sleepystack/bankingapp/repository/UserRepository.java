c700d697fe824e3256130e3b8ca7f281917e9ede
Chirag <chirag2006chopra@gmail.com>
2025-08-07 19:07:36 +0530

    Update UserRepository.java

--- src/main/java/com/sleepystack/bankingapp/repository/UserRepository.java
+++ src/main/java/com/sleepystack/bankingapp/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.sleepystack.bankingapp.repository;
 import com.sleepystack.bankingapp.model.User;
+import com.sleepystack.bankingapp.model.enums.UserStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -10,4 +12,5 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByPublicIdentifier(String publicId);
     Optional<User> findByEmail(String email);
     Optional<User> findByPhone(String phone);
+    List<User> findAllByStatus(UserStatus status);
 }
