c624f3416b8822e318a69923d7aeaf6d20274772
Chirag <chirag2006chopra@gmail.com>
2025-08-07 19:07:59 +0530

    Update AccountTypeRepository.java

--- src/main/java/com/sleepystack/bankingapp/repository/AccountTypeRepository.java
+++ src/main/java/com/sleepystack/bankingapp/repository/AccountTypeRepository.java
@@ -1,11 +1,15 @@
 package com.sleepystack.bankingapp.repository;
+import com.sleepystack.bankingapp.model.Account;
 import com.sleepystack.bankingapp.model.AccountType;
+import com.sleepystack.bankingapp.model.enums.AccountStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountTypeRepository extends MongoRepository<AccountType, String> {
     Optional<AccountType> findByPublicIdentifier(String publicIdentifier);
+    List<Account> findAllByUserIdAndStatus(String userId, AccountStatus status);
 }
