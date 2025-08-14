1d55442c930381b05aa92b91972480694a2b7574
Chirag <chirag2006chopra@gmail.com>
2025-08-14 12:56:47 +0530

    adding rate limiting
    adding rate limiting

---
+++ src/main/java/com/sleepystack/bankingapp/filter/RateLimitFilter.java
@@ -0,0 +1,38 @@
+package com.sleepystack.bankingapp.filter;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class RateLimitFilter implements Filter {
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+    throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        String clientIp = request.getRemoteAddr();
+        Bucket bucket = buckets.computeIfAbsent(clientIp, k ->
+                        Bucket.builder()
+                            .addLimit(Bandwidth.classic(10, Refill.greedy(20, Duration.ofMinutes(1))))
+                            .build()
+        );
+        if (bucket.tryConsume(1)) {
+            chain.doFilter(req, res);
+        } else {
+            ((HttpServletResponse) res).setStatus(429);
+            res.getWriter().write("Too Many Requests");// Too Many Requests
+        }
+
+    }
+}
